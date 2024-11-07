aa599bda0a755c5fdba02670f3a850c9390c60c7
fuRan <NgeKaworu@gmail.com>
2024-11-07 14:35:20 +0800

    chore: 译文不用必填

--- app/flashcard/src/model/record.go
+++ app/flashcard/src/model/record.go
@@ -2,7 +2,7 @@
  * @Author: fuRan NgeKaworu@gmail.com
  * @Date: 2023-03-19 03:04:11
  * @LastEditors: fuRan NgeKaworu@gmail.com
- * @LastEditTime: 2024-10-28 13:47:40
+ * @LastEditTime: 2024-11-07 14:31:42
  * @FilePath: /honghuang/app/flashcard/src/model/record.go
  * @Description:
  *
@@ -20,16 +20,16 @@ import (
 const TRecord = "t_record"
 
 type Record struct {
-	ID          *primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty" `                                                           // id
-	UID         *primitive.ObjectID `json:"uid,omitempty" bson:"uid,omitempty" `                                                          // 所有者id
-	CreateAt    *time.Time          `json:"createAt,omitempty" bson:"createAt,omitempty" `                                                // 创建时间
-	UpdateAt    *time.Time          `json:"updateAt,omitempty" bson:"updateAt,omitempty" `                                                // 更新时间
-	ReviewAt    *time.Time          `json:"reviewAt,omitempty" bson:"reviewAt,omitempty" `                                                // 复习时间
-	CooldownAt  *time.Time          `json:"cooldownAt" bson:"cooldownAt" `                                                                // 冷却时间
-	Source      string              `json:"source,omitempty" bson:"source,omitempty" validate:"required_without=ID" label:"原文"`           // 原文
-	Translation string              `json:"translation,omitempty" bson:"translation,omitempty" validate:"required_without=ID" label:"译文"` // 译文
-	InReview    bool                `json:"inReview,omitempty" bson:"inReview" `                                                          // 是否在复习中
-	Exp         int64               `json:"exp,omitempty" bson:"exp" `                                                                    // 复习熟练度
-	Tag         string              `json:"tag" bson:"tag" `                                                                              // 标签
-	Mode        int64               `json:"mode" bson:"mode" `                                                                            // 模式: 0: 关键字; 1: 全文
+	ID          *primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty" `                                                 // id
+	UID         *primitive.ObjectID `json:"uid,omitempty" bson:"uid,omitempty" `                                                // 所有者id
+	CreateAt    *time.Time          `json:"createAt,omitempty" bson:"createAt,omitempty" `                                      // 创建时间
+	UpdateAt    *time.Time          `json:"updateAt,omitempty" bson:"updateAt,omitempty" `                                      // 更新时间
+	ReviewAt    *time.Time          `json:"reviewAt,omitempty" bson:"reviewAt,omitempty" `                                      // 复习时间
+	CooldownAt  *time.Time          `json:"cooldownAt" bson:"cooldownAt" `                                                      // 冷却时间
+	Source      string              `json:"source,omitempty" bson:"source,omitempty" validate:"required_without=ID" label:"原文"` // 原文
+	Translation string              `json:"translation,omitempty" bson:"translation,omitempty"`                                 // 译文
+	InReview    bool                `json:"inReview,omitempty" bson:"inReview" `                                                // 是否在复习中
+	Exp         int64               `json:"exp,omitempty" bson:"exp" `                                                          // 复习熟练度
+	Tag         string              `json:"tag" bson:"tag" `                                                                    // 标签
+	Mode        int64               `json:"mode" bson:"mode" `                                                                  // 模式: 0: 关键字; 1: 全文
 }
